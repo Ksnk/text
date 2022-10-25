6e7c41d3360ec7f823b14579d295c3d0fa5ae0f4
korjakin_s <korjakin_s@kadis.ru>
2022-10-25 16:01:52 +0300

    Refactoring

--- src/tpl.php
+++ src/tpl.php
@@ -8,7 +8,7 @@ use \Exception;
  * @method static string prop($LL, $valute = FALSE, $kop = FALSE) - сумма прописью, с копейками или без
  * @method static string text($template, $data) - текстовая замена, одинарные фигурные скобки
  * @method static string utext($template, $data) - текстовая замена, двойные фигурные скобки
- * @method static string sql($template, $data,$type='') - подготовленое выражение sql.
+ * @method static string sql($template, $data,$type='',$quote=null) - подготовленое выражение sql.
  * @method static string pl($n,$one, $two, $five) - окончание числительных (plural form).
  * @method static string rusd($daystr = null, $format = "j F, Y г.") - дата с месяцами - днями недели по русски.
  */
